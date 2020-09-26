ba029a6b1a92b4a82c1935e8d02dad92b5dd9db2
BalaRohan Kotni <71187552+BalaRohanKotni@users.noreply.github.com>
2020-09-26 22:35:59 +0530

    Create Allocation.py

---
+++ 2020/Round A/Allocation.py
@@ -0,0 +1,19 @@
+t = int(input())
+tc = 0
+
+while t>0:
+  n,b = map(int, input().split())
+  a = list(map(int, input().split()))
+  count = 0
+  a.sort()
+
+  for i in a:
+    if i<=b:
+      b -= i
+      count += 1
+
+  tc += 1
+
+  print("Case #" + str(tc) + ": " + str(count))
+
+  t -=1
